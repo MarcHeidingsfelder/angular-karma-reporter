f23c33b67bbfb604964ea585d6d4e072797b5458
MarcHeidingsfelder <marc.heidingsfelder@gmail.com>
2023-02-12 01:01:30 +0100

    test(Plugin) add first tests for plugin factory

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,40 @@
+import { BaseReporterDecoratorFactory } from '../types/baseReporterDecorator';
+import * as plugin from './../index';
+
+describe('reporter factory', () => {
+
+    const getFactory = () => {
+        return module.default['reporter:angular'][1];
+    }
+
+    let baseReporterDecoratorFactoryMock: Partial<BaseReporterDecoratorFactory>;
+    let configurationMock: any;
+    let loggerMock: any;
+    let module: any;
+
+    beforeEach(() => {
+        baseReporterDecoratorFactoryMock = () => { };
+        configurationMock = {
+            htmlReporter: {}
+        };
+        loggerMock = {
+            create: () => { }
+        };
+        module = plugin as any;
+    })
+
+    it('should provide the factory function', () => {
+        const factory = getFactory();
+        const typeOfFactory = typeof factory;
+        expect(typeOfFactory).toBe('function');
+    });
+
+    it('should return an instance of AngularReporter', () => {
+        const factory = getFactory();
+        const reporter = factory(
+            baseReporterDecoratorFactoryMock as BaseReporterDecoratorFactory, configurationMock, loggerMock, undefined, undefined);
+        expect(reporter).toBeTruthy();
+    });
+
+
+});
